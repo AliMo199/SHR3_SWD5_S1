00a1d1a6f80acf4c61b4dd1b05097325b27453e7
Ali Elgamal <alimelgamal199@gmail.com>
2025-09-08 23:36:43 +0300

    ADDED assignment files

---
+++ Linq&EF day 03/ConsoleApp/ConsoleApp/ApplicationDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS; Initial Catalog=DEPIAssign; Integrated Security=true; TrustServerCertificate=True");
+
+        public DbSet<Project> Projects { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
---
+++ Linq&EF day 03/ConsoleApp/ConsoleApp/ProjectEntityConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class ProjectEntityConfiguration : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+
+            builder.Property(p => p.Id)
+            .UseIdentityColumn(seed: 10, increment: 10);
+
+            builder.Property(p => p.Name)
+            .HasMaxLength(50)
+            .HasDefaultValue("OurProject")
+            .IsRequired();
+
+            builder.Property(p => p.Cost)
+            .HasColumnType("money");
+            builder.ToTable("Projects");
+            builder.HasCheckConstraint("CK_Project_Cost_Range",
+            "[Cost] >= 500000 AND [Cost] <= 3500000"
+            );
+        }
+    }
+}
