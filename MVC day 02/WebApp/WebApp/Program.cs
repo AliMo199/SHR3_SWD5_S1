ec14bd2532f27c888f4c994a31a7dad2e7cc0f1b
Ali Elgamal <alimelgamal199@gmail.com>
2025-09-23 00:00:29 +0300

    Added project files

---
+++ MVC day 02/WebApp/WebApp/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<ApplicationDBContext>(options =>
+        options.UseSqlServer("Data Source=MSI\\SQLEXPRESS;Database=DepiAssignment;Trusted_Connection=True;TrustServerCertificate=True;"));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
