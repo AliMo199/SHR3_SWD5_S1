516f424ead6be352c795557aa152d86c4730b6d5
Ali Elgamal <alimelgamal199@gmail.com>
2025-09-29 10:49:32 +0300

    ADDED all Assignment files

---
+++ MVC day 04/WebApp/WebApp/ViewModels/DepartmentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using WebApp.Models;
+
+namespace WebApp.ViewModels
+{
+    public class DepartmentViewModel
+    {
+        public string DepartmentName { get; set; } = string.Empty;
+        public List<Student> StudentsAbove25 { get; set; } = new List<Student>();
+        public List<Student> Students { get; set; } = new List<Student>();
+        public string DepartmentState { get; set; } = string.Empty;
+    }
+}
---
+++ MVC day 04/WebApp/WebApp/ViewModels/StudentDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using WebApp.Models;
+
+namespace WebApp.ViewModels
+{
+    public class StudentDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int DepartmentId { get; set; }
+        public List<Department>? departments { get; set; }
+
+        }
+}
