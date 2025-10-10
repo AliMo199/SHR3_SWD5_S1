e0d0f4638094d9570ef9a32ceb69bbdea3a3a19f
Ali Elgamal <alimelgamal199@gmail.com>
2025-10-10 15:22:15 +0300

    ADDED project files

---
+++ MVC day 06/WebApp/WebApp/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range(5, 70)]
+        public int Age { get; set; }
+
+        public int DepartmentId { get; set; }
+        public Department? Department { get; set; }
+
+        public ICollection<StudentCourses> StudentCourses { get; set; } = new List<StudentCourses>();
+    }
+}
---
+++ MVC day 06/WebApp/WebApp/ViewModels/CourseViewModel.cs
@@ -0,0 +1,11 @@
+﻿using WebApp.Models;
+
+namespace WebApp.ViewModels
+{
+    public class CourseViewModel
+    {
+        Course course = new Course();
+        List<Department> departments = new List<Department>();
+
+    }
+}
