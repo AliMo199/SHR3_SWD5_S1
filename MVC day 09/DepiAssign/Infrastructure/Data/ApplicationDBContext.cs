d577e52602d372b2390d1272c9f004504bacb7ae
Ali Elgamal <alimelgamal199@gmail.com>
2025-10-23 22:32:22 +0300

    ADDED Project and Required files.

---
+++ MVC day 09/DepiAssign/Infrastructure/Data/ApplicationDBContext.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TaskItem> TaskItems { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=MSI\\SQLEXPRESS;Initial Catalog=Depi;Integrated Security=True;Trust Server Certificate=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TaskItem>(entity =>
+            {
+                entity.HasKey(e => e.id);
+                entity.Property(e => e.Title).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.Description).HasMaxLength(500);
+                entity.Property(e => e.IsCompleted).HasDefaultValue(false);
+                entity.Property(e => e.CreatedAt).HasDefaultValueSql("GETDATE()");
+            });
+        }
+    }
+}
---
+++ MVC day 09/DepiAssign/Infrastructure/Implementation/TaskRepository.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Models;
+using Domain.Repositories;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Implementation
+{
+    public class TaskRepository : ITaskRepository
+    {
+        public ApplicationDBContext _Context;
+        public TaskRepository(ApplicationDBContext context)
+        {
+            _Context = context;
+        }
+        public List<TaskItem> GetAll()
+        {
+            return _Context.TaskItems.ToList();
+        }
+        public TaskItem GetbyId(int id)
+        {
+            return _Context.TaskItems
+                .FirstOrDefault(t=>t.id == id);
+        }
+        public void Create(TaskItem Task)
+        {
+            _Context.TaskItems.Add(Task);
+            _Context.SaveChanges();
+        }
+        public void Delete(TaskItem Task)
+        {
+            _Context.TaskItems.Remove(Task);
+            _Context.SaveChanges();
+        }
+        public void Update(TaskItem Task)
+        {
+            _Context.TaskItems.Update(Task);
+            _Context.SaveChanges();
+        }
+    }
+}
