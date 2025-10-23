e0d6589dff144a137f0fdbaba2747274f5095949
Ali Elgamal <alimelgamal199@gmail.com>
2025-10-23 22:10:59 +0300

    ADDED project and Required files

---
+++ MVC day 08/DepiAssign/Domain/Models/TaskItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class TaskItem
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Completed")]
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? DueDate { get; set; }
+    }
+}
---
+++ MVC day 08/DepiAssign/Domain/Repositories/ITaskRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public interface ITaskRepository
+    {
+        List<TaskItem> GetAll();
+        TaskItem GetbyId(int id);
+        void Create(TaskItem task);
+        void Update(TaskItem task);
+        void Delete(TaskItem task);
+    }
+}
